0265376a0affe53ddf776a509184db222305ee59
ryan <ryan@ryans-air.myfiosgateway.com>
2023-07-27 21:01:34 -0400

    missing files

---
+++ AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  PawPlanner
+//
+//  Created by ryan on 7/27/23.
+//
+
+import SwiftUI
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // Request user's permission to send notifications
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (allowed, error) in
+            // Handle errors if needed
+        }
+        
+        return true
+    }
+    
+    // Handle received notification when app is in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Show the notification alert and play the sound
+        completionHandler([.banner, .sound])
+    }
+}
